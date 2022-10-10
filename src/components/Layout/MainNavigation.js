5c6b81c866df26b239e2b9064ac2adbc3e540493
aliciams <# apt-get install git>
2022-10-10 11:47:04 +0200

    first commit

---
+++ src/components/Layout/MainNavigation.js
@@ -0,0 +1,45 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import AuthContext from '../../store/auth-context';
+
+import classes from './MainNavigation.module.css';
+
+const MainNavigation = () => {
+  const authCtx = useContext(AuthContext);
+
+  const isLoggenIn = authCtx.isLoggenIn;
+
+  const logoutHandler = () => {
+    authCtx.logout();
+  };
+
+  return (
+    <header className={classes.header}>
+      <Link to='/'>
+        <div className={classes.logo}>React Auth</div>
+      </Link>
+      <nav>
+        <ul>
+          {!isLoggenIn && (
+            <li>
+            <Link to='/auth'>Login</Link>
+          </li>
+          )}
+          {isLoggenIn && (
+            <li>
+            <Link to='/profile'>Profile</Link>
+          </li>
+          )}
+          {isLoggenIn && (
+            <li>
+            <button onClick={logoutHandler}>Logout</button>
+          </li>
+          )}
+          
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
